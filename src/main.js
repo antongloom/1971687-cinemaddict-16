33e6bfe8a3cfdd3e9c023687834840e8237fc459
Anton <kleshnev.aa@infotek.ru>
2021-12-17 12:23:18 +0300

    commit 9

--- src/main.js
+++ src/main.js
@@ -3,33 +3,54 @@ import {createSiteListTemplate} from './view/site-list-view'
 import {createSiteProfileTemplate} from './view/site-profile-view'
 import {createSiteBtnShowTemplate} from './view/site-btn-show-view'
 import {createSitePopupTemplate} from './view/site-popup-view'
-import {RenderPosition, renserTemplate} from './render.js'
+import {RenderPosition, rensedTemplate} from './render.js'
 import {generateTask, generatePopup} from './mock/task.js'
 import {generateFilter} from './mock/filter.js'
 
-const TASK_COUNT = 5;
+const TASK_COUNT = 20
+const TASK_COUNT_PER_STEP = 5
 
 const tasks = Array.from({length: TASK_COUNT}, generateTask)
 const popup = generatePopup()
 
 const siteProfileElement = document.querySelector('.header')  
 
-renserTemplate(siteProfileElement, createSiteProfileTemplate(), RenderPosition.BEFOREEND)
+rensedTemplate(siteProfileElement, createSiteProfileTemplate(), RenderPosition.BEFOREEND)
 
 
 const siteMainElement = document.querySelector('.main')
 const siteListTemplate = siteMainElement.querySelector('.films-list__container')
 const siteBtnShowTemplate = siteMainElement.querySelector('.films-list')
 
-renserTemplate(siteMainElement, createSiteMenuTemplate(generateFilter, tasks), RenderPosition.BEFOREBEGIN)
+rensedTemplate(siteMainElement, createSiteMenuTemplate(generateFilter, tasks), RenderPosition.BEFOREBEGIN)
 
-for (let i = 0; i < TASK_COUNT; i++) {
-    renserTemplate(siteListTemplate, createSiteListTemplate(tasks[i]), RenderPosition.AFTERBEGIN)
+for (let i = 0; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+    rensedTemplate(siteListTemplate, createSiteListTemplate(tasks[i]), RenderPosition.AFTERBEGIN)
 }
 
-renserTemplate(siteBtnShowTemplate, createSiteBtnShowTemplate(), RenderPosition.BEFOREEND)
+if (tasks.length > TASK_COUNT_PER_STEP) {
+
+    let renderedTaskCount = TASK_COUNT_PER_STEP
+
+    rensedTemplate(siteBtnShowTemplate, createSiteBtnShowTemplate(), RenderPosition.BEFOREEND)
+    const loadMoreButton = siteBtnShowTemplate.querySelector('.films-list__show-more')
+
+    loadMoreButton.addEventListener('click', (evt) => {
+        evt.preventDefault()
+        tasks
+            .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+            .forEach((task) => rensedTemplate(siteListTemplate, createSiteListTemplate(task), RenderPosition.AFTERBEGIN))
+
+        renderedTaskCount += TASK_COUNT_PER_STEP
+
+        if (renderedTaskCount >= tasks.length) {
+            loadMoreButton.remove()
+        }
+
+    })
+}
 
 
 const sitePopupTemplate = document.querySelector('.footer')
 
-//renserTemplate(sitePopupTemplate, createSitePopupTemplate(popup), RenderPosition.BEFOREEND)
+//rensedTemplate(sitePopupTemplate, createSitePopupTemplate(popup), RenderPosition.BEFOREEND)
